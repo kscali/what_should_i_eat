314eb72fcf1de2a5a0192cfb7419236294570db9
Koychian Saeteurn <kosatun@aol.com>
2018-11-15 14:30:24 -0800

    moved code for output into another file

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,13 +1,13 @@
+require 'what_should_i_eat/printing'
 require 'net/http'
 require 'json'
 
 module WhatShouldIEat
-  def self.fresh
+  def WhatShouldIEat.fresh
     result = Net::HTTP.get('www.edamam.com', '/search?type=Feeds')
     recipes = JSON.parse(result)
     name = recipes.first['items'].first['label']
     url = recipes.first['items'].first['url']
-    puts name
-    puts url
+    Printing.print name, url
   end
 end
